44ee07162d070cfab108532e8e3f2e3b101ca5e3
Jan Richter <dzejar@dzejar.net>
2016-10-12 09:05:11 -0700

    prediction module enhancements
    stats:
     * training by 100k samples takes ~25 minutes and generates 658MB classifier
     * training with 50k samples takes ~6 minutes, generates 299MB big classifier

--- flights/prediction.py
+++ flights/prediction.py
@@ -19,34 +19,54 @@ def minute_of_day(dt):
 def map_to_int(s):
     # simple hash from str to numeric value for mining inputs
     # TODO: use something better, maybe OneHotEncoder?
+    #       better solution might be assigning numeric ids based on list of categories by avg delay 
+    #       but could we be then sure we won't get unknown airport/airline?
     return abs(hash(s)) % (10 ** 6)
 
 
 def extract_features(flight):
+    """
+    creates feature vector for training and prediction inputs
+    """
     return [
             minute_of_day(flight.scheduled_departure),
             map_to_int(flight.carrier),
-            # simple_int(flight.departure_airport),
             flight.src_airport.latitude,
-            flight.src_airport.longitude
+            flight.src_airport.longitude,
+            map_to_int(flight.arrival_airport),
+            int(flight.flight_number) # we might use just len() of flight number
            ]
 
 
-def train_from_db(limit_sample=10000):
+def train_from_db(limit_sample=50000):
+    """
+    get a sample from db and train calssifier
+    """
+    # TODO: use some more representative sample than just first n of db records
+    # NOTE: SVM training complexity is O(n^2), we cannot use whole dataset for training, 
+    #       is tehre a better solution?
     sample = models.Flight.query.limit(limit_sample).all()
     data, target = zip(*[(extract_features(f), f.delay_mins) for f in sample])
     clf.fit(data, target)
     
 
-def test_prediction_db(tests=100, offset=5000000):
+def test_prediction_db(tests=1000, offset=200000):
+    """
+    test our classifier's prediction (in)accuracy
+    """
     test_flights = models.Flight.query.offset(offset).limit(tests).all()
     score = 0
+    print("{:>6} {:>6} {:>7}".format("pred.", "actual", "error"))
+    print("-" * 21)
     for f in test_flights:
         r = clf.predict(np.array(extract_features(f)).reshape((1, -1)))
-        error = abs(r[0] - f.delay_mins)
-        score += error
-        print(r, f.delay_mins, error)
+        error = (r[0] - f.delay_mins)
+        score += abs(error)
+        print("{:>6} {:>6} {:>7}".format(r[0], f.delay_mins, error))
+    print("-" * 21)
     print("sum errors: %s" % score)
+    print("average error: %s" % (score/tests))
+    print("-" * 21)
 
 
 def store_classifier_model(filename="model.dat"):
@@ -56,3 +76,4 @@ def store_classifier_model(filename="model.dat"):
 def restore_classifier_model(filename="model.dat"):
     global clf
     clf = joblib.load(filename)
+
